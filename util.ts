0a2fd746a635a043d13e3a6e57a2a4be119f5aab
Joakim Deak <joakim.deak@surikat.se>
2022-06-02 20:50:47 +0200

    did a silly goofy thing

---
+++ util.ts
@@ -0,0 +1,24 @@
+const START = 1630845000000;
+const DAY = 86400000;
+const HOUR = 3600000;
+const MINUTE = 60000;
+
+export const getDuration = () => {
+  let diff = Date.now() - START;
+
+  let days, hours, minutes;
+
+  if (diff >= DAY) {
+    days = Math.floor(diff / DAY);
+    diff -= days * DAY;
+  }
+  if (diff >= HOUR) {
+    hours = Math.floor(diff / HOUR);
+    diff -= hours * HOUR;
+  }
+  if (diff >= MINUTE) {
+    minutes = Math.floor(diff / MINUTE);
+    diff -= minutes * MINUTE;
+  }
+  return `${days} days ${hours} hours ${minutes} minutes`;
+};
