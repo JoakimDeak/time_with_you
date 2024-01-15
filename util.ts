1ed764c26b432a663f9d96cfb5ef2071d3719d78
opera-joakimd <joakimd@opera.com>
2024-01-15 14:30:13 +0100

    Fix handling of 0 h/m/s

--- util.ts
+++ util.ts
@@ -1,24 +1,26 @@
-const START = 1630845000000;
-const DAY = 86400000;
-const HOUR = 3600000;
-const MINUTE = 60000;
+const START = 1630845000000
+const DAY = 86400000
+const HOUR = 3600000
+const MINUTE = 60000
 
 export const getDuration = () => {
-  let diff = Date.now() - START;
+  let diff = Date.now() - START
 
-  let days, hours, minutes;
+  let days = 0,
+    hours = 0,
+    minutes = 0
 
   if (diff >= DAY) {
-    days = Math.floor(diff / DAY);
-    diff -= days * DAY;
+    days = Math.floor(diff / DAY)
+    diff -= days * DAY
   }
   if (diff >= HOUR) {
-    hours = Math.floor(diff / HOUR);
-    diff -= hours * HOUR;
+    hours = Math.floor(diff / HOUR)
+    diff -= hours * HOUR
   }
   if (diff >= MINUTE) {
-    minutes = Math.floor(diff / MINUTE);
-    diff -= minutes * MINUTE;
+    minutes = Math.floor(diff / MINUTE)
+    diff -= minutes * MINUTE
   }
-  return `${days} days ${hours} hours ${minutes} minutes`;
-};
+  return `${days} days ${hours} hours ${minutes} minutes`
+}
